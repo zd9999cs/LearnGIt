23db49165e6a75e017b1a6e76d00edb6a2aca00f
OmbreKala <zd9899@gmail.com>
2019-10-12 10:39:57 +0800

    First Commit

---
+++ Learngit/Learngit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learngit
+//
+//  Created by 佘恩泽 on 2019/10/12.
+//  Copyright © 2019 OmbreKala. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
